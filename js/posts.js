9623d44074ac9965fa59538fbbd56a8dc9acee37
Jesus Mimila Hurtado <166437832+JesusMimilaHurtado@users.noreply.github.com>
2024-06-28 09:54:00 -0700

    added post structure
    added post structure

--- js/posts.js
+++ js/posts.js
@@ -1,36 +1,86 @@
 /* Posts Page JavaScript */
 
 "use strict";
-const imgSrc = localStorage.getItem('profileImg')
 
-document.addEventListener('DOMContentLoaded', () => {
-    img.src = imgSrc
-}) 
+//profile image
+const imgSrc = localStorage.getItem("profileImg");
 
-function getLoginData() {
-  const loginJSON = window.localStorage.getItem("login-data");
-  return JSON.parse(loginJSON) || {};
-}
+//new post button
+const newPostButton = document.getElementById("addPost");
+
+//stored values
+const token = localStorage.getItem("token");
+
+//posts container
+const posts = document.getElementById('posts')
+
+document.addEventListener("DOMContentLoaded", () => {
+  img.src = imgSrc;
+  newPostButton.addEventListener("click", createNewPosts);
+  displayPost();
+});
 
 function displayPost() {
+  const myHeaders = new Headers();
+  myHeaders.append("accept", "application/json");
+  myHeaders.append("Authorization", `Bearer ${token}`);
 
-    const userData = getLoginData();
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-    const myHeaders = new Headers();
-    myHeaders.append("accept", "application/json");
-    myHeaders.append("Authorization", userData.token);
+  fetch(
+    "http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts?limit=30&offset=0",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      data.forEach(post => {
+        posts.innerHTML += `
+        <div class="row mb-4 justify-content-center">
+            <div class="col-sm-11 col-md-8 col-lg-8 col-xl-4 mt-3">
+              <div class="card">
+                <div class="card-body pt-0"
+                  <h4 class="mountain-desc card-text">${post.username} </h4>
+                  <hr class="w-100">
+                  <strong class="card-text">${post.text}</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+            `;
+      });
+    })
+    .catch((error) => console.error(error));
+}
 
-    const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow",
-    };
+function createNewPosts() {
+  const newPost = document.getElementById("new_post");
 
-    fetch(
-        "http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts?limit=100&offset=0",
-        requestOptions
-    )
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.error(error));
+  const myHeaders = new Headers();
+  myHeaders.append("accept", "application/json");
+  myHeaders.append("Authorization", `Bearer ${token}`);
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    'text' : newPost,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(
+    "http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
 }
